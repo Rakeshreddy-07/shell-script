1ab543e5ad812c4d86286b8246f42e48c0d0ccfd
Rakeshreddy-07 <rakeshreddynarra07@gmail.com>
2025-01-02 13:43:27 +0530

    variable1

---
+++ 07_install-script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+
+uid=$(id -u)
+if [ $uid -ne 0 ]; then
+    echo "user does not have the required permission to run the script, use sudo access to run the script"
+else
+    echo "user has the required permission to run the script"
+    exit 1
+fi 
+
+#install mysql rpm
+dnf install mysql -y
+
+
+
