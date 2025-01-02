6f632d585bcffa4391b86c2d47b4b7ab10a45488
Rakeshreddy-07 <rakeshreddynarra07@gmail.com>
2025-01-02 12:48:42 +0530

    variable1

--- 04_arrays.sh
+++ 04_arrays.sh
@@ -2,8 +2,8 @@
 
 movies=("pushpa" "rrr" "devara")
 
-echo "first movie: $movies[0]"
-echo "second movie: $movies[1]"
-echo "third movie: $movies[2]"
+echo "first movie: ${movies[0]}"
+echo "second movie: ${movies[1]}"
+echo "third movie: ${movies[2]}"
 
-echo "All movies list: $movies[$@]"
+echo "All movies list: ${movies[$@]}"
