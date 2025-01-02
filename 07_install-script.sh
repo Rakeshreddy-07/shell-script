0208d7cce0c436ec2c9127d7f98d5df0345d7128
Rakeshreddy-07 <rakeshreddynarra07@gmail.com>
2025-01-02 14:07:08 +0530

    variable1

--- 07_install-script.sh
+++ 07_install-script.sh
@@ -5,6 +5,7 @@
 uid=$(id -u)
 if [ $uid -ne 0 ]; then
     echo "user does not have the required permission to run the script, use sudo access to run the script"
+    exit 1
 else
     echo "user has the required permission to run the script, excuting script now"
 fi 
