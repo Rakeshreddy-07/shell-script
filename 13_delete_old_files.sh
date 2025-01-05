913d7afeac5d57597b46144dec39cd253a87abf7
Rakeshreddy-07 <rakeshreddynarra07@gmail.com>
2025-01-05 17:31:54 +0530

    shell

---
+++ 13_delete_old_files.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+#colors
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+#log file location
+timestamp=$(date +%Y-%m-%d-%H:%M:%S)
+log_location="/var/log/expense_logs"
+log_file=$(echo $0 | cut -d "." -f1)
+log_file_name="$log_location/$log_file-$timestamp.log"
+
+
+
+mkdir -p /var/log/shellscript_logs
+
+
+#check user id sudo access to run the script
+CHECK_ROOT(){
+uid=$(id -u)
+    if [ $uid -ne 0 ]; then
+        echo "user does not have the required permission to run the script, use sudo access to run the script"
+        exit 1
+    else
+        echo "user has the required permission to run the script, excuting script now"
+    fi 
+}
+
+#To validate the install process
+VALIDATE () {
+    if [ $1 -eq 0 ]; then
+        echo -e "$2.. $G SUCCESS $N"
+    else
+        echo -e "$2.. $R FAILURE $N"
+        exit 1
+    fi    
+}
+
+#calling this function to check if sudo access is enabled or not
+CHECK_ROOT
+
+find $log_location -name "*.log" -mtime +30 >> $HOME/$log_file.txt
+
+while read -r filename 
+do
+    ls $filename
+done << $HOME/$log_file.txt
