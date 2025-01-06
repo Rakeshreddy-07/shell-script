ee2460a346b1ee5496378bcb3cf502896a616b8c
Rakeshreddy-07 <rakeshreddynarra07@gmail.com>
2025-01-06 20:02:31 +0530

    shell

--- 14_old_logs.sh
+++ 14_old_logs.sh
@@ -14,20 +14,10 @@ log_file_name="$log_location/$log_file-$timestamp.log"
 
 source_dir=$1
 dest_dir=$2
+days=${3:-14}
 
 #mkdir -p /var/log/expense_logs
 
-
-#check user id sudo access to run the script
-CHECK_ROOT(){
-uid=$(id -u)
-    if [ $uid -ne 0 ]; then
-        echo "user does not have the required permission to run the script, use sudo access to run the script"
-        exit 1
-    else
-        echo "user has the required permission to run the script, excuting script now"
-    fi 
-}
 
 if [ $# -lt 2 ]; then
     echo -e "$R USAGE: $N $0 <source_directory> <destination_directory> "
@@ -36,9 +26,9 @@ else
     echo "executing the script"
 fi
 
-echo "validating the directories"
+#validating the directories
 
-if [ ! -d $source_dir ]; then
+if [ ! -d $1 ]; then
     echo "$source_dir does not exists"
 else    
     echo "$source_dir exists"
