71f66e43cea8be5780c68faf46c6dbe2b2b41a4e
Rakeshreddy-07 <rakeshreddynarra07@gmail.com>
2025-01-07 22:08:24 +0530

    shell

--- 15-monitoring.sh
+++ 15-monitoring.sh
@@ -12,8 +12,5 @@ while read -r line
 do
     usage=$( echo $line | awk -F " " '{ print $6f }' | cut -d "%" -f 1 )
     partition=$( echo $line | awk -F " " '{ print $Nf }' )
-
+    echo "Partition: $partition Usage: $usage"
 done <<< $disk
-
-echo $usage
-echo $partition
