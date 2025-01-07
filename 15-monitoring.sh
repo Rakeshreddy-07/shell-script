20d4053d0a64e8f8113bf9ce6a3631bb17a96ed4
Rakeshreddy-07 <rakeshreddynarra07@gmail.com>
2025-01-07 22:11:01 +0530

    shell

--- 15-monitoring.sh
+++ 15-monitoring.sh
@@ -10,7 +10,7 @@ threshold=5
 
 while read -r line
 do
-    usage=$( echo $line | awk -F " " '{ print $6f }' | cut -d "%" -f 1 )
-    partition=$( echo $line | awk -F " " '{ print $Nf }' )
+    usage=$(echo $line | awk -F " " '{ print $6f }' | cut -d "%" -f 1)
+    partition=$(echo $line | awk -F " " '{ print $7f }')
     echo "Partition: $partition Usage: $usage"
 done <<< $disk
