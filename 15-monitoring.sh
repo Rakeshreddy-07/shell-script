5bfee4c64d982c53e093ee871f3e1f1f05a926b3
Rakeshreddy-07 <rakeshreddynarra07@gmail.com>
2025-01-07 22:06:37 +0530

    shell

--- 15-monitoring.sh
+++ 15-monitoring.sh
@@ -4,14 +4,14 @@
 
 #echo $USAGE
 
-disk=$(df -hT | grep -i xfs)
+disk=$(df -hT | grep xfs)
 threshold=5
 
 
 while read -r line
 do
     usage=$( echo $line | awk -F " " '{ print $6f }' | cut -d "%" -f 1 )
-    partition=$( echo $line | awk -F " " '{ print $nf }' )
+    partition=$( echo $line | awk -F " " '{ print $Nf }' )
 
 done <<< $disk
 
